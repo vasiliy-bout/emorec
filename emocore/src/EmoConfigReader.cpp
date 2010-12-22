83baab6219add206497ed698b47a7bac68d00286
Vasiliy Bout <bout.vasiliy@gmail.com>
2010-12-22 15:36:58 +0300

    Config reader has been added

---
+++ emocore/src/EmoConfigReader.cpp
@@ -0,0 +1,79 @@
+
+#include <string.h>
+
+#include "EmoConfigReader.h"
+#include "EmoErrors.h"
+
+
+
+bool EmoConfigReader::loadClasses(const std::string &fileName, std::map<std::string, std::string> &properties) {
+	EmoConfigReader reader(properties);
+	if (!reader.readDocument(fileName)) {
+		return false;
+	}
+	return !reader.isInterrupted();
+}
+
+EmoConfigReader::EmoConfigReader(std::map<std::string, std::string> &properties) : myProperties(properties) {
+	myState = READ_NOTHING;
+}
+
+
+
+static const std::string TAG_ROOT = "config";
+static const std::string TAG_PROPERTY = "property";
+
+
+void EmoConfigReader::startElementHandler(const char *tag, const char **attributes) {
+	switch (myState) {
+	case READ_NOTHING:
+		if (TAG_ROOT.compare(tag) == 0) {
+			myState = READ_CONFIG;
+		} else {
+			interrupt();
+		}
+		break;
+	case READ_CONFIG:
+		if (TAG_PROPERTY.compare(tag) == 0) {
+			myState = READ_PROPERTY;
+			const char *name = attributeValue(attributes, "name");
+			const char *value = attributeValue(attributes, "value");
+			if (name == 0 || value == 0) {
+				interrupt();
+			} else {
+				std::string nameStr(name);
+				std::string valueStr(value);
+				myProperties[name] = value;
+			}
+		} else {
+			interrupt();
+		}
+		break;
+	case READ_PROPERTY:
+		interrupt();
+		break;
+	}
+}
+
+void EmoConfigReader::endElementHandler(const char *tag) {
+	switch (myState) {
+	case READ_NOTHING:
+		interrupt();
+		break;
+	case READ_CONFIG:
+		if (TAG_ROOT.compare(tag) == 0) {
+			myState = READ_NOTHING;
+		} else {
+			interrupt();
+		}
+		break;
+	case READ_PROPERTY:
+		if (TAG_PROPERTY.compare(tag) == 0) {
+			myState = READ_CONFIG;
+		} else {
+			interrupt();
+		}
+		break;
+	}
+}
+
---
+++ emocore/src/EmoConfigReader.h
@@ -0,0 +1,33 @@
+
+#ifndef __EMOCONFIGREADER_H__
+#define __EMOCONFIGREADER_H__
+
+#include <vector>
+
+#include "xml/EmoXMLReader.h"
+
+
+
+class EmoConfigReader : public EmoXMLReader {
+
+	static bool loadClasses(const std::string &fileName, std::map<std::string, std::string> &properties);
+
+private:
+	EmoConfigReader(std::map<std::string, std::string> &properties);
+
+public:
+	void startElementHandler(const char *tag, const char **attributes);
+	void endElementHandler(const char *tag);
+
+private:
+	std::map<std::string, std::string> &myProperties;
+
+	enum {
+		READ_NOTHING,
+		READ_CONFIG,
+		READ_PROPERTY,
+	} myState;
+};
+
+
+#endif /* __EMOCONFIGREADER_H__ */
